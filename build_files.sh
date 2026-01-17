1b4f2561d4f3f2c7a730a95702263adfe7cee830
adhilgafoor <adhilgafoor44@gmail.com>
2026-01-17 07:46:24 +0530

    Complete UI Overhaul: Glassmorphism for Auth, Admin, Shop & Fixes

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-echo "BUILD START"
-python3 -m pip install -r requirements.txt
-echo "PIP FINISHED"
-# python3 manage.py makemigrations --noinput
-# python3 manage.py migrate --noinput
-# echo "MIGRATIONS FINISHED"
-python3 manage.py collectstatic --noinput --clear
-echo "BUILD END"
--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
+    path('', views.loginindex),
     path('loginindex', views.loginindex),
     path('logoutss', views.logoutss),
     path('adminhome', views.admin_home),
