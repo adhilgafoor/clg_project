07b098d2e46be869088c436f9124e913c05f1dbb
adhilgafoor <adhilgafoor44@gmail.com>
2026-01-17 06:17:02 +0530

    Fix line endings and build script

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+echo "PIP FINISHED"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+echo "MIGRATIONS FINISHED"
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
